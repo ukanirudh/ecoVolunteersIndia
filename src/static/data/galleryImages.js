ff0853165d2bce178942fd9c346e391c488b0fe4
U K ANIRUDH <anirudh.anirudh93@gmail.com>
2019-01-17 21:37:32 +0530

    Anirudh : Adding new images

--- src/static/data/galleryImages.js
+++ src/static/data/galleryImages.js
@@ -4,5 +4,41 @@ export const galleryImages = [
   },
   {
     imageUrl: 'vol2.jpg'
+  },
+  {
+    imageUrl: 'bat4.jpg'
+  },
+  {
+    imageUrl: 'bat5.jpg'
+  },
+  {
+    imageUrl: 'bat6.jpg'
+  },
+  {
+    imageUrl: 'bat7.jpg'
+  },
+  {
+    imageUrl: 'bat8.jpg'
+  },
+  {
+    imageUrl: 'bat9.jpg'
+  },
+  {
+    imageUrl: 'bat10.jpg'
+  },
+  {
+    imageUrl: 'bat11.jpg'
+  },
+  {
+    imageUrl: 'bat12.jpg'
+  },
+  {
+    imageUrl: 'bat13.jpg'
+  },
+  {
+    imageUrl: 'evi11.jpg'
+  },
+  {
+    imageUrl: 'evi22.jpg'
   }
 ]
