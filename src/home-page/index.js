cb6731c71bef9f75dc133a689b9a162c87bef698
U K ANIRUDH <anirudh.anirudh93@gmail.com>
2019-04-30 19:36:42 +0530

    Anirudh : Adding another cover image

--- src/home-page/index.js
+++ src/home-page/index.js
@@ -75,39 +75,36 @@ const HomepageLayout = (props) => (
         </Grid.Row>
       </Grid>
     </Segment>
-    <Segment style={{ padding: '8em 0em' }} vertical>
-      <Container text>
-        <Header as='h3' style={{ fontSize: '2em' }}>
-          Breaking The Grid, Grabs Your Attention
-        </Header>
-        <p style={{ fontSize: '1.33em' }}>
-          Instead of focusing on content creation and hard work, we have learned how to master the
-          art of doing nothing by providing massive amounts of whitespace and generic content that
-          can seem massive, monolithic and worth your attention.
-        </p>
-        <Button as='a' size='large'>
-          Read More
-        </Button>
-        <Divider
-          as='h4'
-          className='header'
-          horizontal
-          style={{ margin: '3em 0em', textTransform: 'uppercase' }}
-        >
-          <a href='#'>Case Studies</a>
-        </Divider>
-        <Header as='h3' style={{ fontSize: '2em' }}>
-          Did We Tell You About Our Bananas?
-        </Header>
-        <p style={{ fontSize: '1.33em' }}>
-          Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but
-          it's really true. It took years of gene splicing and combinatory DNA research, but our
-          bananas can really dance.
-        </p>
-        <Button as='a' size='large'>
-          I'm Still Quite Interested
-        </Button>
-      </Container>
+    <Segment placeholder>
+      <Grid columns={2} stackable textAlign='center'>
+        <Grid.Row verticalAlign='middle'>
+          <Grid.Column>
+            <Image bordered rounded size='large' src={require('../static/images/coverimage_1.jpg')} />
+          </Grid.Column>
+          <Grid.Column>
+            <Segment style={{ padding: '8em 0em' }} vertical>
+              <Header as='h3' style={{ fontSize: '2em' }}>
+                Breaking The Grid, Grabs Your Attention
+              </Header>
+              <p style={{ fontSize: '1.33em' }}>
+                Instead of focusing on content creation and hard work, we have learned how to master the
+                art of doing nothing by providing massive amounts of whitespace and generic content that
+                can seem massive, monolithic and worth your attention.
+              </p>
+              <Button as='a' size='large'> Read More </Button>
+              <Header as='h3' style={{ fontSize: '2em' }}>
+                Did We Tell You About Our Bananas?
+              </Header>
+              <p style={{ fontSize: '1.33em' }}>
+                Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but
+                it's really true. It took years of gene splicing and combinatory DNA research, but our
+                bananas can really dance.
+              </p>
+              <Button as='a' size='large'> I'm Still Quite Interested </Button>
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </Segment>
     <AppFooter />
   </ResponsiveContainer>
